44cb079119a07700c982b8ccab62ae5522a9d486
edjunma <edjunma@gmail.com>
2021-05-25 13:20:18 -0400

    create endpoint with express

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/projects', async(req, res) => {
+  const videos = await getVideos()
+  res.json(videos)
+})
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
 

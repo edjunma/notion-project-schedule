91747d20e0b53226ad8c0cb17a4fc57437a89a95
edjunma <edjunma@gmail.com>
2021-05-25 13:11:49 -0400

    add express server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const express = require('express')
 const getProjects = require('./services/notion')
+const PORT = process.env.PORT || 5000
 
-;(async () => {
-  const nProjects = await getProjects()
-  console.log(nProjects)
-})()
+const app = express()
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
+

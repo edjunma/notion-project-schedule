5b708d81204cd5b82cb9becfbca3a33df55508ea
edjunma <edjunma@gmail.com>
2021-05-31 02:46:13 -0400

    finishing up frontend

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/projects', async(req, res) => {
-  const videos = await getVideos()
-  res.json(videos)
+  const projects = await getProjects()
+  res.json(projects)
 })
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,35 @@
-const videosEl = document.querySelector('#videos');
+const projectsEl = document.querySelector('#projects');
 const loadingEl = document.querySelector('#loading');
 let loading = false;
 
-const getVideosFromBackend = async () => {
+const getProjectsFromBackend = async () => {
   loading = true;
-  const res = await fetch('https://localhost:5000/videos');
+  const res = await fetch('https://localhost:5000/projects');
   const data = await res.json();
   loading = false;
   return data;
 }
+
+const addProjectsToDom = async () => {
+  const projects = await getProjectsFromBackend()
+  
+  if (!loading) {
+    loadingEl.innerHTML = ''
+  }
+
+  projects.forEach(project => {
+    const div = document.createElement('div');
+    div.className = 'project'
+    div.innerHTML = `
+      <h3>${project.title}</h3>
+      <ul>
+        <li><strong>Release Date: </strong> ${project.date}</li>
+        <li><strong>Description: </strong> ${project.description}</li>
+      </ul>
+      <div class="tags">${project.tags}</div>
+      `
+      projectsEl.appendChild(div);
+  })
+}
+
+addVideosToDom();
